09e7c4845666594977c216f00f710a9d606f44b1
Cynthia <cynthiazhou98@gmail.com>
2018-02-16 15:51:08 -0800

    updated with library

--- opticalsensor.ino
+++ opticalsensor.ino
@@ -1,79 +1,53 @@
 //TCRT5000 analog pins
 int IRPin_1 = 1; 
+int IRVal_1;
 int IRPin_2 = 2; 
-int IRPin_3 = 3; 
-int IRPin_4 = 4; 
-int IRValues[4];
+int IRVal_2;
 
 void setup() {
   Serial.begin(9600);
-  IRValues[0] = IRValues[1] = IRValues[2] = IRValues[3] = 0;
 }
 
 void loop() {
-  readIR(); 
+  readIR();
+  processIR();
 }
 
 void readIR(){
-  IRValues[0] = analogRead(IRPin_1);
-  IRValues[1] = analogRead(IRPin_2);
-  IRValues[2] = analogRead(IRPin_3);
-  IRValues[3] = analogRead(IRPin_4);
-  
-  Serial.println(IRValues[2]);
+  IRVal_1 = analogRead(IRPin_1);
+  IRVal_2 = analogRead(IRPin_2);
+}
 
+void processIR(){
   Serial.print("Sensor 1: ");
-   switch(checkBlackOrWhite(IRValues[0])) {
+   switch(checkBlackOrWhite(IRVal_1)) {
     case 1:
       Serial.println("black");
+      while (IRVal_1==1){
+        smoothTurn(1,1,what);
+      }
       break;
     case 0:
       Serial.println("white");
       break;
-    default: 
-      break;
+    default: break;
   }
   
   Serial.print("Sensor 2: ");
-   switch(checkBlackOrWhite(IRValues[1])) {
+   switch(checkBlackOrWhite(IRVal_2)) {
     case 1:
       Serial.println("black");
+      while (IRVal_2==1){
+        smoothTurn(1,1,what);
+      }
       break;
     case 0:
       Serial.println("white");
       break;
-    default: 
-      break;
+    default: break;
   }
-  
-  Serial.print("Sensor 3: ");
-   switch(checkBlackOrWhite(IRValues[2])) {
-    case 1:
-      Serial.println("black");
-      break;
-    case 0:
-      Serial.println("white");
-      break;
-    default: 
-      break;
-  }
-
-  Serial.print("Sensor 4: ");
-   switch(checkBlackOrWhite(IRValues[3])) {
-    case 1:
-      Serial.println("black");
-      break;
-    case 0:
-      Serial.println("white");
-      break;
-    default: 
-      break;
-  }
-
-  delay(1000);
 }
 
-//returns whether or not the detected sensor value represents black or white
 byte checkBlackOrWhite(int val){
   if (val > 500) {
     return 1;
