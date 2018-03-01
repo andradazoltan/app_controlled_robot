9e01ae60e731ba793dc0d71b1c4f4517866f4cf7
Andrada Zoltan <andrada.12321@gmail.com>
2018-03-01 09:09:09 -0800

    Created main file

---
+++ Main_Arduino/Main_Arduino.ino
@@ -0,0 +1,14 @@
+#include "line_following.h"
+#include "obstacle_avoiding.h"
+#include "ultrasonic_sensor.h"
+#include "wheelZ.h"
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
